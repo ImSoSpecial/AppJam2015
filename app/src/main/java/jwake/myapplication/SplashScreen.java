b5ea348eba32816acf4dc09febe13649da31ba07
Jonathan Mak <mak.jona@gmail.com>
2015-04-15 22:29:11 -0700

    Splash screen displays for 3 seconds
    then goes to home screen

--- app/src/main/java/jwake/myapplication/SplashScreen.java
+++ app/src/main/java/jwake/myapplication/SplashScreen.java
@@ -1,9 +1,11 @@
 package jwake.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.os.Handler;
 
 
 public class SplashScreen extends ActionBarActivity {
@@ -12,6 +14,16 @@ public class SplashScreen extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, HomeScreen.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
     }
 
 
