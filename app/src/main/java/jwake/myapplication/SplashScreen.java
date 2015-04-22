b60a000842e8479b5c953d094e41bda06c85a717
ImSoSpecial <adrian90893@yahoo.com>
2015-04-22 01:51:24 -0700

    New changes

--- app/src/main/java/jwake/myapplication/SplashScreen.java
+++ app/src/main/java/jwake/myapplication/SplashScreen.java
@@ -1,38 +1,48 @@
 package jwake.myapplication;
 
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
+import android.widget.ImageView;
 
 
 public class SplashScreen extends Activity {
+
+    ImageView iv1;
+    AnimationDrawable Anim;
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        new Handler().postDelayed(new Runnable() {
+        mp = MediaPlayer.create(SplashScreen.this, R.raw.splashsound);
+        iv1 = (ImageView) findViewById(R.id.splashView);
+        iv1.setBackgroundResource(R.drawable.animatesplash);
 
+        Anim = (AnimationDrawable) iv1.getBackground(); //new AnimationDrawable();
+        Anim.start();
+        mp.start();
+
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+
                 SharedPreferences introFirstTime = getSharedPreferences("introInfo", Context.MODE_PRIVATE);
                 Intent i;
-                if(introFirstTime.getBoolean("introInfo", false) == true) {
+                if (introFirstTime.getBoolean("introInfo", false) == true) {
                     i = new Intent(SplashScreen.this, HomeScreen.class);
-                }
-
-                else{
+                } else {
                     i = new Intent(SplashScreen.this, IntroScreen.class);
                 }
-
                 startActivity(i);
-                finish();
+                //finish();
 
             }
         }, 3000);
